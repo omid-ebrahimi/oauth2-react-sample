e1e6f86947bb2fdefc791156953f363e2acac0f1
omid-ebrahimi <ebrahimi.omid69@gmail.com>
2019-05-18 15:05:13 +0430

    wraps App by store provider

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import './App.css';
 import MainRouter from "./routes/MainRouter";
+import Provider from './data/store/provider'
 
 function App() {
     return (
-        <BrowserRouter>
-            <main>
-                <MainRouter/>
-            </main>
-        </BrowserRouter>
+        <Provider>
+            <BrowserRouter>
+                <main>
+                    <MainRouter/>
+                </main>
+            </BrowserRouter>
+        </Provider>
     );
 }
 
