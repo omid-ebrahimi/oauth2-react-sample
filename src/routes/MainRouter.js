986d6140ecd1614c050462c345f57865ecbd07a1
omid-ebrahimi <ebrahimi.omid69@gmail.com>
2019-06-04 16:39:49 +0430

    changes folder structure

--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import PrivateRoute from './PrivateRoute/container';
-import NotFound from "../screens/NotFound";
-import Home from "../screens/Home";
-import Login from "../screens/Login/container";
-import Protected from "../screens/Protected";
+import NotFound from "../app/components/Main/screens/NotFound";
+import Home from "../app/components/Main/screens/Home";
+import Login from "../app/components/Main/screens/Login/container";
+import Protected from "../app/components/Main/screens/Protected";
 
 const MainRouter = () => (
     <Switch>
